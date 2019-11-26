1d7874dc0010a83d892f67fdfc93c1d37450bbe7
Adam Flöhr <adam.flohr@slu.se>
2019-11-26 16:29:27 +0100

    Tests on Rmd many too one

--- Scripts/Original to many in one.R
+++ Scripts/Original to many in one.R
@@ -2,20 +2,21 @@
 composition <- data.frame(component = slidesFromRmd("Test/test1.Rmd"),
                           main_slide = 1,
                           row = c(1,1,1,2,2,2),
-                          col = c(1,2,3,1,2,3))
+                          col = c(1,2,3,1,2,3),
+                          stringsAsFactors = F)
 
 mainslides <- data.frame(name = "first test slide")
 
 rmd_org <- readLines("Test/test1.Rmd")
 
 composition$first_line <- firstinstance(composition$component, rmd_org)
-composition$last_line <- c(composition$first_line[-1], length(rmd_org))
+composition$last_line <- c(composition$first_line[-1], length(rmd_org)) - 1
 
 library(dplyr)
 composition %>%
   group_by(main_slide) %>% 
-  mutate(x1 = round(100 * (row - 1) / max(row), 2),
-         y1 = round(100 * (col - 1) / max(col), 2),
+  mutate(y1 = round(100 * (row - 1) / max(row), 2),
+         x1 = round(100 * (col - 1) / max(col), 2),
          height = round(100 * 1 / max(row), 2),
          width = round(100 * 1 / max(col), 2)) %>% 
   ungroup() -> composition
@@ -30,21 +31,35 @@ composition %>%
 
 rmd_new <- rmd_org[1:min(composition$first_line - 1)]
 
-added_main <- c("##", 
-                "", 
-                "<div style='position:absolute;height:700px;width:1300px;top:-20%;left:-20%;background-color:#f0f;font-size:1vw;'>", 
-                "")
-
 for(i in 1:dim(composition_mains)[1]){
   components <- composition %>% filter(main_slide == i)
+  
+  added_main <- c("##", 
+                  "", 
+                  "<div style='position:absolute;height:700px;width:1300px;top:-20%;left:-20%;background-color:#fee;font-size:1vw;'>", 
+                  "")
+  
   for(j in 1:dim(components)[1]){
-    comp <- paste0("<div class='fragment fade-in fade-out' style='position:absolute;height:",
+    comp <- paste0("<div class='fragment fade-in' style='position:absolute;height:",
                    components$height[j], "%;width:",
                    components$width[j], "%;top:",
                    components$y1[j], "%;left:",
                    components$x1[j], "%;'>")
-    title <- paste0("<h2> ", rev(rev(components$component[j])[-c(1:3)]), " </h2>") # DEPENDS ON ALL TITLES HAVING THREE LEADING SYMBOLS
-    added_main <- c(added_main)
+    
+    title <- paste0("<h2> ", substr(components$component[j], 4, 1000), " </h2>") # DEPENDS ON ALL TITLES HAVING THREE LEADING SYMBOLS
+    
+    slide <- rmd_org[(components$first_line[j] + 1) : components$last_line[j]]
+    
+    added_main <- c(added_main, comp, title, slide, "</div>")
   }
   
+  added_main <- c(added_main, "</div>")
+  
+  rmd_new <- c(rmd_new, added_main)
 }
+
+library(rmarkdown)
+writeLines(rmd_new, con = "Test/rmd_new.Rmd")
+setwd("Test/")
+render(input = "rmd_new.Rmd", output_file = "rmd_new.html")
+setwd("..")
