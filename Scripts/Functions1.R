f805b989ab1473f951ced4d6760271e0b06fb2ff
flr.adam <flr.adam@gmail.com>
2019-11-29 19:02:00 +0100

    Exclude hashes in chunks

--- Scripts/Functions1.R
+++ Scripts/Functions1.R
@@ -1,11 +1,14 @@
 # Present the headers from the markdown file
-# WILL IN CURRENT STATE ALSO GIVE ANY COMMENTS IN R CODE
-slidesFromRmd <- function(file){
+extract_slides <- function(file){
   a <- readLines(file, encoding = "UTF-8")
+  
+  outside_chunk <- !(cumsum(substr(a, 1, 3) == "```") %% 2)
+  a <- a[outside_chunk]
+  
   a[substr(a, 1, 1) == "#"]
 }
 
-firstinstance <- function(x, y){
+locate_first_instance <- function(x, y){
   foo <- function(a, b){
     min(which(a == b))
   }
--- Scripts/Original to many in one.R
+++ Scripts/Original to many in one.R
@@ -1,5 +1,5 @@
 # Stepwise changes to original Rmd to form many slides on one slide
-composition <- data.frame(component = slidesFromRmd("Test/test1.Rmd"),
+composition <- data.frame(component = extract_slides("Test/test1.Rmd"),
                           main_slide = 1,
                           row = c(1,1,1,2,2,2),
                           col = c(1,2,3,1,2,3),
@@ -9,7 +9,7 @@ mainslides <- data.frame(name = "first test slide")
 
 rmd_org <- readLines("Test/test1.Rmd")
 
-composition$first_line <- firstinstance(composition$component, rmd_org)
+composition$first_line <- locate_first_instance(composition$component, rmd_org)
 composition$last_line <- c(composition$first_line[-1], length(rmd_org)) - 1
 
 library(dplyr)
